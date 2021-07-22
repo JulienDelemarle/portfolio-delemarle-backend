5985a1d801a79e9ffdb5be57de385f49cfa73709
yulen59 <j.delemarle@gmail.com>
2021-07-22 16:51:32 +0200

    create backend routes, db

---
+++ data/db.sql
@@ -0,0 +1,177 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for Linux (x86_64)
+--
+-- Host: localhost    Database: portfolio
+-- ------------------------------------------------------
+-- Server version	8.0.25-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Presentation`
+--
+
+DROP TABLE IF EXISTS `Presentation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Presentation` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `text` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Presentation`
+--
+
+LOCK TABLES `Presentation` WRITE;
+/*!40000 ALTER TABLE `Presentation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Presentation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gallery_photo`
+--
+
+DROP TABLE IF EXISTS `gallery_photo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gallery_photo` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) DEFAULT NULL,
+  `link_img` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gallery_photo`
+--
+
+LOCK TABLES `gallery_photo` WRITE;
+/*!40000 ALTER TABLE `gallery_photo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gallery_photo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `parcours`
+--
+
+DROP TABLE IF EXISTS `parcours`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `parcours` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `school` varchar(45) DEFAULT NULL,
+  `enterprise` varchar(45) DEFAULT NULL,
+  `year` varchar(45) NOT NULL,
+  `text` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `parcours`
+--
+
+LOCK TABLES `parcours` WRITE;
+/*!40000 ALTER TABLE `parcours` DISABLE KEYS */;
+INSERT INTO `parcours` VALUES (1,'web developer','Wild Code School',NULL,'2021',NULL);
+/*!40000 ALTER TABLE `parcours` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `skills`
+--
+
+DROP TABLE IF EXISTS `skills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `skills` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `text` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `skills`
+--
+
+LOCK TABLES `skills` WRITE;
+/*!40000 ALTER TABLE `skills` DISABLE KEYS */;
+INSERT INTO `skills` VALUES (1,'rigoureux',''),(2,'perséverant',NULL),(3,'',NULL),(4,'',NULL),(5,'',NULL),(6,'',NULL);
+/*!40000 ALTER TABLE `skills` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `softskills`
+--
+
+DROP TABLE IF EXISTS `softskills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `softskills` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `softskills`
+--
+
+LOCK TABLES `softskills` WRITE;
+/*!40000 ALTER TABLE `softskills` DISABLE KEYS */;
+/*!40000 ALTER TABLE `softskills` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `technologies`
+--
+
+DROP TABLE IF EXISTS `technologies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `technologies` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `text` varchar(45) DEFAULT NULL,
+  `picto` varchar(45) DEFAULT NULL,
+  `level` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `technologies`
+--
+
+LOCK TABLES `technologies` WRITE;
+/*!40000 ALTER TABLE `technologies` DISABLE KEYS */;
+INSERT INTO `technologies` VALUES (1,'react',NULL,'home/julien/www/cp4/assets','junior'),(6,'nodeJS',NULL,NULL,'junior'),(7,'express',NULL,NULL,'junior'),(8,'javascript',NULL,NULL,'junior'),(9,'html5',NULL,NULL,'junior'),(10,'css3',NULL,NULL,'junior');
+/*!40000 ALTER TABLE `technologies` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-22 15:24:09
--- index.js
+++ index.js
@@ -1,15 +1,49 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
 
-app.get('/characters', async (req, res) => {
-  res.status(404).send('Route not found! ');
+const PORT = 5050;
+
+const cors = require('cors');
+const { db } = require('./conf');
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/home', async (req, res) => {
+  const [rows] = await db.query('SELECT text FROM Presentation');
+  res.status(200).json(rows);
 });
 
-app.use('/', (req, res) => {
-  res.status(404).send('Route not found! ');
+/* app.get('/api/skills', async (req, res) => {
+  const trackId = req.params.id;
+
+  const [results] = await db.query('SELECT * FROM track WHERE id = ?', [
+    trackId,
+  ]);
+  if (results.length) res.json(results[0]);
+  else res.status(404).send('track not found');
 });
 
-app.listen(5050, () => {
-  console.log('Terra Battle API now available on http://localhost:5050 !');
+app.post('/api/tracks', async (req, res) => {
+  const { title, youtube_url } = req.body;
+  await db.query('INSERT INTO track(title, youtube_url) VALUES(?,?)', [
+    title,
+    youtube_url,
+  ]);
+  res
+    .status(201)
+    .send(`Received new message, you posted: ${title} at ${youtube_url}`);
 });
+*/
+
+app.listen(PORT, (err) => {
+  // eslint-disable-next-line no-console
+  if (err) console.error(err);
+  // eslint-disable-next-line no-console
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
+
+// Please keep this module.exports app, we need it for the tests !
+module.exports = app;
